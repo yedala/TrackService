c3325eb8ceb16c6fa7f839c3c427927e586300dd
aravind <aravindkumar.reddy@cars24.com>
2025-02-11 17:37:02 +0530

    Intial changes

--- routes/expenseRoute.js
+++ routes/expenseRoute.js
@@ -1,14 +1,9 @@
 import { Router } from "express";
 import { createExpense } from "../controllers/expenseController.js";
 import authGaurd from "../middleware/authMiddleware.js";
-// import authGaurd from "../middleware/authMiddleware";
 
 const router = Router();
 
-// router.post("/addExpense", authGaurd);
-// router.route("/all").get(authGaurd, allUsers);
 router.route("/addExpense").post(authGaurd, createExpense);
-// router.route("/token").post(updateToken);
-// router.route("/logout").post(authGaurd, logoutUser);
 
 export default router;
