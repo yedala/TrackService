e94d060c8affd1ab5880fcfd3a9b77261ea98e42
aravind <aravindkumar.reddy@cars24.com>
2025-02-11 17:58:29 +0530

    Intial changes

--- server.js
+++ server.js
@@ -3,12 +3,19 @@ import express, { json } from "express";
 import configDb from "./config/db.js";
 import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
 import expenseRoutes from "./routes/expenseRoute.js";
+import rateLimit from "express-rate-limit";
 
 const app = express();
 
 app.use(json());
+const authLimiter = rateLimit({
+  windowMs: 15 * 60 * 1000,
+  max: 50,
+  message:
+    "Too many authentication requests from this IP, please try again after 15 minutes",
+});
 //my routes
-app.use("/expenses", expenseRoutes);
+app.use("/expenses", authLimiter, expenseRoutes);
 
 //middleware
 app.use(notFound);
