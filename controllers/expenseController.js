fb377ba584c4e0d71112d1f9947b6cb92ea17644
aravind <aravindkumar.reddy@cars24.com>
2025-02-12 10:12:51 +0530

    expense-factory

--- controllers/expenseController.js
+++ controllers/expenseController.js
@@ -21,17 +21,25 @@ const createExpense = asyncHandler(async (req, res) => {
   }
 });
 
-const getAllExpenses = asyncHandler(async (req, res) => {
-  let { userId } = req.body;
+const filterExpenses = asyncHandler(async (req, res) => {
+  const query = req.query;
+  const { userId } = req.body;
+  let filter = { userId: userId };
+  if (query?.startDate || query?.endDate) filter.createdAt = {};
+  if (query?.startDate) filter.createdAt.$gte = new Date(query.startDate);
+  if (query?.endDate)
+    filter.createdAt.$lte = new Date(query.endDate).setHours(23, 59, 59, 999);
+  if (query?.category) filter.category = query.category;
   try {
-    const expenses = await Expense.find({ userId });
+    const expenses = await Expense.find(filter).sort({ updatedAt: -1 });
     res.status(200).json({
       message: "Expenses fetched successfully",
       expenses: expenses,
+      length: expenses.length,
     });
   } catch (e) {
     res.status(400);
     throw new Error("Error occured");
   }
 });
-export { createExpense, getAllExpenses };
+export { createExpense, filterExpenses };
--- routes/expenseRoute.js
+++ routes/expenseRoute.js
@@ -1,13 +1,13 @@
 import { Router } from "express";
 import {
   createExpense,
-  getAllExpenses,
+  filterExpenses,
 } from "../controllers/expenseController.js";
 import authGaurd from "../middleware/authMiddleware.js";
 
 const router = Router();
 
 router.route("/addExpense").post(authGaurd, createExpense);
-router.route("/").get(authGaurd, getAllExpenses);
+router.route("/").get(authGaurd, filterExpenses);
 
 export default router;
